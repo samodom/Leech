a96fdf0131012eec24d8708102c3657ca03fa61d
Sam Odom <samodom@me.com>
2014-02-22 14:29:59 -0600

    Initial Commit

---
+++ Leech/Leech.h
@@ -0,0 +1,13 @@
+//
+//  Leech.h
+//  Leech
+//
+//  Created by Sam Odom on 2/22/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Leech : NSObject
+
+@end
---
+++ Leech/Leech.m
@@ -0,0 +1,13 @@
+//
+//  Leech.m
+//  Leech
+//
+//  Created by Sam Odom on 2/22/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import "Leech.h"
+
+@implementation Leech
+
+@end
