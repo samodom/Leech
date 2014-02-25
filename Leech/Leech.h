63bc797be2a32b84b029f916a6a2d46bd601b45d
Sam Odom <samodom@me.com>
2014-02-25 13:27:41 -0600

    Added umbrella header

---
+++ Leech/Leech.h
@@ -0,0 +1,18 @@
+//
+//  Leech.h
+//  Leech
+//
+//  Created by Sam Odom on 2/25/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#ifndef Leech_Leech_h
+#define Leech_Leech_h
+
+#import "LTTMockNotificationCenter.h"
+#import "LTTFakeNotificationDispatchEntry.h"
+#import "LTTMethodSwizzler.h"
+
+#import "UIViewController+Leech"
+
+#endif
