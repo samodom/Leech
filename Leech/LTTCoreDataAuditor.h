084dffe48abfc44306961b63204023d9635d9677
Sam Odom <samodom@phunware.com>
2014-04-18 23:46:22 -0500

    Added appledoc style comments to some headers

--- Leech/LTTCoreDataAuditor.h
+++ Leech/LTTCoreDataAuditor.h
@@ -6,18 +6,80 @@
 //  Copyright (c) 2014 Swagger Soft. All rights reserved.
 //
 
-typedef void (^core_data_perform_t)(void);
-
 #import <CoreData/CoreData.h>
+
+/**
+ LTTCoreDataAuditor assists in testing code that performs Core Data operations as a synchronous or asynchronous block.
+ 
+ ##Usage
+ 
+ __Step 1__ Begin auditing one of the perform block methods
+ 
+     [LTTCoreDataAuditor auditPerformBlock];
+     //  or [LTTCoreDataAuditor auditPerformBlockAndWait];
+
+ __Step 2__ Invoke your method that performs a block of Core Data operations
+ 
+ __Step 3__ Check that a block has been used
+ 
+     core_data_perform_t blockToPerform = [LTTCoreDataAuditor blockToPerform];
+     XCTAssertNotNil(blockToPerform,
+                     @"The Core Data operations should be performed in a block");
+ 
+ __Step 4__ Check that the expected operations have been performed
+ 
+     blockToPerform();  //  execute the captured block
+     //  various assertions to check outcome of Core Data operations
+     //  assertion checking that managed object context was saved:
+     //  XCTAssertFalse(managedObjectContext.hasChanges, ...);
+ 
+ __Step 5__ End auditing of the perform block methods and clear captured data
+ 
+     [LTTCoreDataAuditor stopAuditingPerformBlock];
+     //  or [LTTCoreDataAuditor stopAuditingPerformBlockAndWait];
+ 
+ */
 
 @interface LTTCoreDataAuditor : NSObject
 
+/**
+ Block type for Core Data testing
+ */
+typedef void (^core_data_perform_t)(void);
+
+
+/** @name Auditing perform block methods */
+
+/**
+ Audits performing of Core Data operations in an asynchronous block
+ 
+ @discussion This method replaces the real method implementation of `-[NSManagedObject performBlock:]` with another method that captures the block to perform and does not perform the block
+ */
 + (void)auditPerformBlock;
+
+/**
+ Ends auditing of asynchronous perform block methods and clears captured data
+ */
 + (void)stopAuditingPerformBlock;
 
+/**
+ Audits performing of Core Data operations in a synchronous block
+ 
+ @discussion This method replaces the real method implementation of `-[NSManagedObject performBlockAndWait:]` with another method that captures the block to perform and does not perform the block
+ */
 + (void)auditPerformBlockAndWait;
+
+/**
+ Ends auditing of synchronous perform block methods and clears captured data
+ */
 + (void)stopAuditingPerformBlockAndWait;
 
+
+/** @name Accessing audited block to perform */
+
+/**
+ @return core_data_perform_t block that has been captured
+ */
 + (core_data_perform_t)blockToPerform;
 
 @end
--- Leech/LTTTimeFreezer.h
+++ Leech/LTTTimeFreezer.h
@@ -8,11 +8,63 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ LTTTimeFreezer provides a way to *freeze time* during testing so that code attempting to use the current date and time can be given an arbitrary time for the duration of the test.
+ 
+ ##Usage
+
+ __Step 1__ Freeze time
+ 
+    [LTTTimeFreezer freezeTime];
+
+ __Step 2__ (Optional) Set a custom date if needed for manipulation of the current time
+ 
+    [LTTTimeFreezer setFrozenDate:<#otherDate#>];
+
+ __Step 3__ Invoke your method that uses the current time (or some derivation thereof).  For example:
+ 
+    MyClass *myObject = [MyClass myMethodUsingTheCurrentTime];
+    XCTAssertEqualObjects(myObject.currentDateProperty,
+                          [NSDate date],  //  or <#otherDate#>
+                          @"The method should set the current date on the object");
+ 
+ __Step 4__ Unfreeze time
+ 
+    [LTTTimeFreezer unfreezeTime];
+
+ @warning Always unfreeze time at the end of a test where you freeze time or other tests may be affected.
+ */
+
 @interface LTTTimeFreezer : NSObject
 
+/** @name Freezing and unfreezing time */
+
+/**
+ * Freezes time at the moment of execution so that calls to `+[NSDate date]` will use the frozen time
+ *
+ * @warning Assertion failure if called when time is already frozen.
+ */
 + (void)freezeTime;
+
+/**
+ * Unfreezes time so that calls to `+[NSDate date]` will again use the appropriate system time
+ *
+ * @warning Assertion failure if called when time is not frozen.
+ */
 + (void)unfreezeTime;
+
+/** @name Checking if time is frozen */
+
+/**
+ * @return BOOL indicating whether or not time has been frozen by LTTTimeFreezer
+ */
 + (BOOL)timeIsFrozen;
+
+/** @name Setting frozen date */
+
+/**
+ * @param newDate Sets frozen date to be returned by calls to `+[NSDate date]`
+ */
 + (void)setFrozenDate:(NSDate*)newDate;
 
 @end
