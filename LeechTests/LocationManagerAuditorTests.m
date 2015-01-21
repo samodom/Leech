e3c8694870ae5439f7311c3254a65cf6a217fe95
Sam Odom <samodom@phunware.com>
2015-01-21 12:26:11 -0600

    Cleaned up location manager auditor tests

--- LeechTests/LocationManagerAuditorTests.m
+++ LeechTests/LocationManagerAuditorTests.m
@@ -15,13 +15,13 @@
 //  Production
 #import "LTTLocationManagerAuditor.h"
 
-
 @interface LocationManagerAuditorTests : XCTestCase
 
 @end
 
 @implementation LocationManagerAuditorTests {
     CLLocationManager *locationManager;
+    IMP realImplementation, currentImplementation;
 }
 
 - (void)setUp {
@@ -32,16 +32,36 @@
 
 - (void)tearDown {
     locationManager = nil;
+    realImplementation = nil;
+    currentImplementation = nil;
 
     [super tearDown];
+}
+
+- (Method)classMethod:(SEL)selector {
+    return class_getClassMethod([CLLocationManager class], selector);
+}
+
+- (Method)instanceMethod:(SEL)selector {
+    return class_getInstanceMethod([CLLocationManager class], selector);
+}
+
+- (IMP)classMethodImplementation:(SEL)selector {
+    Method method = [self classMethod:selector];
+    return method_getImplementation(method);
+}
+
+- (IMP)instanceMethodImplementation:(SEL)selector {
+    Method method = [self instanceMethod:selector];
+    return method_getImplementation(method);
 }
 
 #pragma mark - Region monitoring
 
 - (void)testAuditorOverridesMonitoringAvailabilityForClassMethod {
-    IMP realImplementation = method_getImplementation(class_getClassMethod([CLLocationManager class], @selector(isMonitoringAvailableForClass:)));
+    realImplementation = [self classMethodImplementation:@selector(isMonitoringAvailableForClass:)];
     [LTTLocationManagerAuditor overrideMonitoringAvailable];
-    IMP currentImplementation = method_getImplementation(class_getClassMethod([CLLocationManager class], @selector(isMonitoringAvailableForClass:)));
+    currentImplementation = [self classMethodImplementation:@selector(isMonitoringAvailableForClass:)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
     BOOL monitoringAvailable = [CLLocationManager isMonitoringAvailableForClass:[CLCircularRegion class]];
     XCTAssertFalse(monitoringAvailable, @"The location manager should not indicate monitoring availability by default");
@@ -49,19 +69,19 @@
     monitoringAvailable = [CLLocationManager isMonitoringAvailableForClass:[CLBeaconRegion class]];
     XCTAssertTrue(monitoringAvailable, @"The location manager should remember monitoring availability by class");
     [LTTLocationManagerAuditor reverseMonitoringAvailableOverride];
-    currentImplementation = method_getImplementation(class_getClassMethod([CLLocationManager class], @selector(isMonitoringAvailableForClass:)));
+    currentImplementation = [self classMethodImplementation:@selector(isMonitoringAvailableForClass:)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
 }
 
 - (void)testAuditorCapturesRegionToStartMonitoring {
-    IMP realImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(startMonitoringForRegion:)));
-    IMP realAccessorImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(monitoredRegions)));
+    realImplementation = [self instanceMethodImplementation:@selector(startMonitoringForRegion:)];
+    IMP realAccessorImplementation = [self instanceMethodImplementation:@selector(monitoredRegions)];
     CLLocationCoordinate2D center = CLLocationCoordinate2DMake(24.0, -12.0);
     CLCircularRegion *region = [[CLCircularRegion alloc] initWithCenter:center radius:14.2 identifier:@"circular region"];
     [LTTLocationManagerAuditor auditStartMonitoringForRegionMethod:locationManager];
-    IMP currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(startMonitoringForRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(startMonitoringForRegion:)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
-    IMP currentAccessorImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(monitoredRegions)));
+    IMP currentAccessorImplementation = [self instanceMethodImplementation:@selector(monitoredRegions)];
     XCTAssertNotEqual(currentAccessorImplementation, realAccessorImplementation, @"The method should be swizzled");
     [locationManager startMonitoringForRegion:region];
     CLRegion *capturedRegion = [LTTLocationManagerAuditor regionToStartMonitoring:locationManager];
@@ -69,49 +89,49 @@
     NSSet *monitoredRegions = [locationManager monitoredRegions];
     XCTAssertEqualObjects(monitoredRegions, [NSSet setWithObject:region], @"The monitored regions should be available");
     [LTTLocationManagerAuditor stopAuditingStartMonitoringForRegionMethod:locationManager];
-    currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(startMonitoringForRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(startMonitoringForRegion:)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
-    currentAccessorImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(monitoredRegions)));
+    currentAccessorImplementation = [self instanceMethodImplementation:@selector(monitoredRegions)];
     XCTAssertEqual(currentAccessorImplementation, realAccessorImplementation, @"The method should no longer be swizzled");
 }
 
 - (void)testAuditorAllowsSettingOfMonitoredRegions {
-    IMP realImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(monitoredRegions)));
+    realImplementation = [self instanceMethodImplementation:@selector(monitoredRegions)];
     CLLocationCoordinate2D center = CLLocationCoordinate2DMake(24.0, -12.0);
     CLCircularRegion *regionOne = [[CLCircularRegion alloc] initWithCenter:center radius:14.2 identifier:@"circular region one"];
     CLCircularRegion *regionTwo = [[CLCircularRegion alloc] initWithCenter:center radius:42.8 identifier:@"circular region two"];
     NSSet *regions = [NSSet setWithObjects:regionOne, regionTwo, nil];
     [LTTLocationManagerAuditor setMonitoredRegions:regions forLocationManager:locationManager];
-    IMP currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(monitoredRegions)));
+    currentImplementation = [self instanceMethodImplementation:@selector(monitoredRegions)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
     NSSet *monitoredRegions = [locationManager monitoredRegions];
     XCTAssertEqualObjects(monitoredRegions, regions, @"The monitored regions should be remebered");
     [LTTLocationManagerAuditor clearMonitoredRegionsForLocationManager:locationManager];
-    currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(monitoredRegions)));
+    currentImplementation = [self instanceMethodImplementation:@selector(monitoredRegions)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
 }
 
 - (void)testAuditorCapturesRegionToStopMonitoring {
-    IMP realImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(stopMonitoringForRegion:)));
+    realImplementation = [self instanceMethodImplementation:@selector(stopMonitoringForRegion:)];
     CLLocationCoordinate2D center = CLLocationCoordinate2DMake(24.0, -12.0);
     CLCircularRegion *region = [[CLCircularRegion alloc] initWithCenter:center radius:14.2 identifier:@"circular region"];
     [LTTLocationManagerAuditor auditStopMonitoringForRegionMethod:locationManager];
-    IMP currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(stopMonitoringForRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(stopMonitoringForRegion:)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
     [locationManager stopMonitoringForRegion:region];
     CLRegion *capturedRegion = [LTTLocationManagerAuditor regionToStopMonitoring:locationManager];
     XCTAssertEqualObjects(capturedRegion, region, @"The region to stop monitoring should be captured");
     [LTTLocationManagerAuditor stopAuditingStopMonitoringForRegionMethod:locationManager];
-    currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(stopMonitoringForRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(stopMonitoringForRegion:)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
 }
 
 #pragma mark - Beacon ranging
 
 - (void)testAuditorOverridesRangingAvailabilityMethod {
-    IMP realImplementation = method_getImplementation(class_getClassMethod([CLLocationManager class], @selector(isRangingAvailable)));
+    realImplementation = [self classMethodImplementation:@selector(isRangingAvailable)];
     [LTTLocationManagerAuditor overrideRangingAvailable];
-    IMP currentImplementation = method_getImplementation(class_getClassMethod([CLLocationManager class], @selector(isRangingAvailable)));
+    currentImplementation = [self classMethodImplementation:@selector(isRangingAvailable)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
     BOOL rangingAvailable = [CLLocationManager isRangingAvailable];
     XCTAssertFalse(rangingAvailable, @"The mock location manager should not indicate ranging availability by default");
@@ -119,19 +139,19 @@
     rangingAvailable = [CLLocationManager isRangingAvailable];
     XCTAssertTrue(rangingAvailable, @"The mock location manager should remember ranging availability when set");
     [LTTLocationManagerAuditor reverseRangingAvailableOverride];
-    currentImplementation = method_getImplementation(class_getClassMethod([CLLocationManager class], @selector(isRangingAvailable)));
+    currentImplementation = [self classMethodImplementation:@selector(isRangingAvailable)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
 }
 
 - (void)testAuditorCapturesBeaconRegionToStartRanging {
-    IMP realImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(startRangingBeaconsInRegion:)));
-    IMP realAccessorImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(rangedRegions)));
+    realImplementation = [self instanceMethodImplementation:@selector(startRangingBeaconsInRegion:)];
+    IMP realAccessorImplementation = [self instanceMethodImplementation:@selector(rangedRegions)];
     NSUUID *uuid = [NSUUID UUID];
     CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:uuid.UUIDString];
     [LTTLocationManagerAuditor auditStartRangingBeaconsInRegionMethod:locationManager];
-    IMP currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(startRangingBeaconsInRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(startRangingBeaconsInRegion:)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
-    IMP currentAccessorImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(rangedRegions)));
+    IMP currentAccessorImplementation = [self instanceMethodImplementation:@selector(rangedRegions)];
     XCTAssertNotEqual(currentAccessorImplementation, realAccessorImplementation, @"The method should be swizzled");
     [locationManager startRangingBeaconsInRegion:region];
     CLBeaconRegion *capturedRegion = [LTTLocationManagerAuditor regionToStartRanging:locationManager];
@@ -139,41 +159,41 @@
     NSSet *rangedRegions = [locationManager rangedRegions];
     XCTAssertEqualObjects(rangedRegions, [NSSet setWithObject:region], @"The ranged regions should be available");
     [LTTLocationManagerAuditor stopAuditingStartRangingBeaconsInRegionMethod:locationManager];
-    currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(startRangingBeaconsInRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(startRangingBeaconsInRegion:)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
-    currentAccessorImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(rangedRegions)));
+    currentAccessorImplementation = [self instanceMethodImplementation:@selector(rangedRegions)];
     XCTAssertEqual(currentAccessorImplementation, realAccessorImplementation, @"The method should no longer be swizzled");
 }
 
 - (void)testAuditorCapturesBeaconRegionToStopRanging {
-    IMP realImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(stopRangingBeaconsInRegion:)));
+    realImplementation = [self instanceMethodImplementation:@selector(stopRangingBeaconsInRegion:)];
     NSUUID *uuid = [NSUUID UUID];
     CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:uuid.UUIDString];
     [LTTLocationManagerAuditor auditStopRangingBeaconsInRegionMethod:locationManager];
-    IMP currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(stopRangingBeaconsInRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(stopRangingBeaconsInRegion:)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
     [locationManager stopRangingBeaconsInRegion:region];
     CLBeaconRegion *capturedRegion = [LTTLocationManagerAuditor regionToStopRanging:locationManager];
     XCTAssertEqualObjects(capturedRegion, region, @"The region to stop ranging should be captured");
     [LTTLocationManagerAuditor stopAuditingStopRangingBeaconsInRegionMethod:locationManager];
-    currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(stopRangingBeaconsInRegion:)));
+    currentImplementation = [self instanceMethodImplementation:@selector(stopRangingBeaconsInRegion:)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
 }
 
 - (void)testAuditorAllowsSettingOfRangedRegions {
-    IMP realImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(rangedRegions)));
+    realImplementation = [self instanceMethodImplementation:@selector(rangedRegions)];
     NSUUID *uuid = [NSUUID UUID];
     CLBeaconRegion *regionOne = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:uuid.UUIDString];
     uuid = [NSUUID UUID];
     CLBeaconRegion *regionTwo = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:uuid.UUIDString];
     NSSet *regions = [NSSet setWithObjects:regionOne, regionTwo, nil];
     [LTTLocationManagerAuditor setRangedRegions:regions forLocationManager:locationManager];
-    IMP currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(rangedRegions)));
+    currentImplementation = [self instanceMethodImplementation:@selector(rangedRegions)];
     XCTAssertNotEqual(currentImplementation, realImplementation, @"The method should be swizzled");
     NSSet *rangedRegions = [locationManager rangedRegions];
     XCTAssertEqualObjects(rangedRegions, regions, @"The ranged regions should be remebered");
     [LTTLocationManagerAuditor clearRangedRegionsForLocationManager:locationManager];
-    currentImplementation = method_getImplementation(class_getInstanceMethod([CLLocationManager class], @selector(rangedRegions)));
+    currentImplementation = [self instanceMethodImplementation:@selector(rangedRegions)];
     XCTAssertEqual(currentImplementation, realImplementation, @"The method should no longer be swizzled");
 }
 
